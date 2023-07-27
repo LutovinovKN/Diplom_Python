a421590827de58be758e0d9e44f6848a4e05c9c3
LutovinovKN <VesperLKN@mail.ru>
2023-07-27 12:55:04 +0300

    1. Создал домашнюю страницу; 2. Настроил БД; 3. Создал интерфейс регистрации

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 
 def main():
-
     return render_template('index.html')
+
+@app.route('/showSignUp')
+
+def showSignUp():
+    return render_template('signup.html')
+
 
 
 if __name__ == "__main__":
-
     app.run(port=5002)
 
