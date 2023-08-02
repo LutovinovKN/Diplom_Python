3d4c043810f19f898cb9243de29f8c9ca75de14f
LutovinovKN <VesperLKN@mail.ru>
2023-08-02 20:51:01 +0300

    Сделал выбор поста и возможность удаления поста

--- flask-blog/app.py
+++ flask-blog/app.py
@@ -1,27 +1,70 @@
-from flask import Flask, render_template
+from flask import request, Flask, render_template, redirect   # Import these flask functions
 import sqlite3
 import os
-
-if "mydb.db" not in os.listdir():
-    conn = sqlite3.connect("mydb.db")
-    db = conn.cursor()
-    
-    db.execute("CREATE TABLE posts (title TEXT, content TEXT, \
-        date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
-    conn.commit()
-    conn.close()
+from flask.helpers import flash, url_for
+# print(os.listdir())
+if  "mydb.db" not in os.listdir():
+        conn = sqlite3.connect("mydb.db")
+        db = conn.cursor()
+        db.execute("CREATE TABLE posts (title TEXT, content TEXT, date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+        conn.commit()
+        conn.close()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret key"
 
+@app.route('/create', methods=["GET", "POST"]) # Allowing Post requests
+
+def create_post():
+    if request.method.upper() == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        if title == None or content == None or title.strip() == "" or content.strip() == "":
+            # flashes a message to tell the user to fill all the fields
+            flash("Please fill all the fields")
+            return render_template("create.html")
+
+        conn = sqlite3.connect("mydb.db")
+        cur = conn.cursor()
+        # Adding the post to the database
+        cur.execute("INSERT INTO posts (title, content) VALUES(?, ?)", (title, content))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for("display_posts")) # redirect user
+    return render_template("create.html")
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+        return render_template("index.html")
 
 @app.route("/posts")
 def display_posts():
-    return render_template("posts.html",)
+        conn = sqlite3.connect("mydb.db")
+        cur = conn.cursor()
+        cur.execute("SELECT *, rowid FROM posts") # Getting all the posts from the sqlite3 database
+        posts = cur.fetchall() # fetching all the posts from the database and assigning them to the posts variable
+        cur.close()
+        conn.close()
+        return render_template("posts.html", posts=posts)  # Passing the posts variable ( which is a list ) to the front-end so that jinja2 can loop over it and display all the posts
+
+@app.route("/posts/<int:post_id>")
+def display_post(post_id):
+    conn = sqlite3.connect("mydb.db")
+    cur = conn.cursor()
+    post = cur.execute("SELECT * FROM posts WHERE rowid = ?", (str(post_id))).fetchone() # Notice the fetchone() fucntion
+
+    return render_template("post.html", post=post, post_id=post_id)
+
+@app.route("/posts/<int:post_id>/delete")
+def delete_post(post_id):
+    conn = sqlite3.connect("mydb.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM posts WHERE rowid = ?", (str(post_id)))
+    conn.commit()
+
+    return redirect(url_for("display_posts"))
 
 if __name__ == "__main__":
     app.run(debug=True)
-
